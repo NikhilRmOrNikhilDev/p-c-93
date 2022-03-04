46e949d9485523287b735ee24df536642f75b729
NikhilRmOrNikhilDev <95686653+NikhilRmOrNikhilDev@users.noreply.github.com>
2022-03-04 22:32:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function hide(){
+    document.getElementById("img-loader").id = "img-hide";
+    document.getElementById("row").id = "row-active";
+}
+setTimeout( hide, 3000);
+
+function addUser(){
+
+}
